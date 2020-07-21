b3827c40758749b7f3edeaf505da3807396ddf0d
vrramosm <vf.ramos7@gmail.com>
2020-07-21 19:01:13 -0300

    Criado as pastas de order, product e user na 'infrastructure'

--- back-end/api/routes.js
+++ back-end/api/routes.js
@@ -1,9 +1,9 @@
 const { login, register } = require('../application/user/registerLogin');
 const { profileClient } = require('../application/user/profileClient');
-const { listProducts } = require('../application/products/products');
+const { listProducts } = require('../application/product/products');
 const { adminProfile } = require('../application/user/profileAdmin');
-const { ordersAdmin, getOneOrderAdmin, putStatusOrderAdmin } = require('../application/orders/ordersAdmin');
-const { createOrder, getOrdersClient, getOneOrderClient } = require('../application/orders/ordersClient');
+const { ordersAdmin, getOneOrderAdmin, putStatusOrderAdmin } = require('../application/order/ordersAdmin');
+const { createOrder, getOrdersClient, getOneOrderClient } = require('../application/order/ordersClient');
 
 module.exports = {
   register,
