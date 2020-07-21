b44c8968c6afbf4d9e707fa21cbbfd39e731fbef
vrramosm <vf.ramos7@gmail.com>
2020-07-21 18:51:23 -0300

    Retirado as conexoes do mysql

--- back-end/api/routes.js
+++ back-end/api/routes.js
@@ -1,9 +1,9 @@
-const { login, register } = require('../controllers/registerLogin');
-const { profileClient } = require('../controllers/profileClient');
-const { listProducts } = require('../controllers/products');
-const { adminProfile } = require('../controllers/profileAdmin');
-const { ordersAdmin, getOneOrderAdmin, putStatusOrderAdmin } = require('../controllers/ordersAdmin');
-const { createOrder, getOrdersClient, getOneOrderClient } = require('../controllers/ordersClient');
+const { login, register } = require('../application/user/registerLogin');
+const { profileClient } = require('../application/user/profileClient');
+const { listProducts } = require('../application/products/products');
+const { adminProfile } = require('../application/user/profileAdmin');
+const { ordersAdmin, getOneOrderAdmin, putStatusOrderAdmin } = require('../application/orders/ordersAdmin');
+const { createOrder, getOrdersClient, getOneOrderClient } = require('../application/orders/ordersClient');
 
 module.exports = {
   register,
