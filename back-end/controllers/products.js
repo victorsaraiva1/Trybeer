842332e51c17d0103f2fbe35ef8da9fcf82e35ae
Gabriel Dalseco dos Santos <gabriel_santos96@outlook.com>
2020-07-22 19:20:44 -0300

    removendo pasta controllers

--- back-end/controllers/products.js
+++
@@ -1,7 +0,0 @@
-const products = require('../models/products');
-
-exports.listProducts = async (_req, res) => {
-  const login = await products.getListProduct();
-
-  return res.status(200).json(login);
-};
