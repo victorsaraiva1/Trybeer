19bf5b6b980ca2bbcb73a516f6a1601aabb8d2b7
vrramosm <vf.ramos7@gmail.com>
2020-07-24 16:04:49 -0300

    Criando uma ordem atras do migrate e seed

--- back-end/infrastructure/database/seeders/20200721190603-orders.js
+++ back-end/infrastructure/database/seeders/20200721190603-orders.js
@@ -1,24 +1,21 @@
 'use strict';
 
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
-  },
+  up: async (queryInterface) => (
+    queryInterface.bulkInsert(
+      'Orders',
+      [
+        {
+          user_id: 2,
+          data: new Date(),
+          address: 'Belo Horizonte',
+          address_number: 1001,
+          status: 0,
+        },
+      ],
+      {},
+    )
+  ),
 
-  down: async (queryInterface, Sequelize) => {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  },
+  down: async (queryInterface) => queryInterface.bulkDelete('Orders', null, {}),
 };
