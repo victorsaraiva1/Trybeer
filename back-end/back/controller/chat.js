ded02114be3d5ff93ec84c5adec43373d0f6da24
henriqueeyer <henriqueeyer@outlook.com>
2020-07-28 16:02:12 -0300

    foi

--- back-end/back/controller/chat.js
+++ back-end/back/controller/chat.js
@@ -1,9 +1,7 @@
 const Chat = require('../model/chat');
 
 const returnMessage = async (data) => {
-  if (!data.lastErrorObject.updatedExisting) {
-    return Chat.getOneChatById(data.lastErrorObject.upserted)
-  }
+  if (!data.lastErrorObject.updatedExisting) return Chat.getOneChatById(data.lastErrorObject.upserted);
   const { _id: id } = data.value;
   return Chat.getOneChatById(id);
 };
